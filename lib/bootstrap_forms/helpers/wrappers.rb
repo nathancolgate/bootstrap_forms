40a6fbe752a25cafa9f1242ee1658bd7319720d7
Nathan Colgate <nathancolgate@gmail.com>
2012-11-05 13:09:44 -0800

    Commenting out required attribute

--- lib/bootstrap_forms/helpers/wrappers.rb
+++ lib/bootstrap_forms/helpers/wrappers.rb
@@ -59,9 +59,10 @@ module BootstrapForms
       end
 
       def required_attribute
-        if respond_to?(:object) and object.respond_to?(:errors) and object.class.respond_to?('validators_on')
-          return { :required => true } if object.class.validators_on(@name).any? { |v| v.kind_of? ActiveModel::Validations::PresenceValidator }
-        end
+        # This is messing up my file_fields
+        # if respond_to?(:object) and object.respond_to?(:errors) and object.class.respond_to?('validators_on')
+        #   return { :required => true } if object.class.validators_on(@name).any? { |v| v.kind_of? ActiveModel::Validations::PresenceValidator }
+        # end
         {}
       end
 
